fe1936c7aca18fc90b67e1b6213356bccd3e5b7f
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-22 10:41:21 +0200

    nuovotesto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Branch nuovoTesto</Text>
+      <Text>nuovoTesto</Text>
       <StatusBar style="auto" />
     </View>
   );
