26986aebd84707de41c30746bcc5d4a867505363
MattiaCas8 <100998981+MattiaCas8@users.noreply.github.com>
2022-06-22 10:21:49 +0200

    nuovoBranch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>GIT </Text>
+      <Text>NuovoBranch </Text>
       <StatusBar style="auto" />
     </View>
   );
